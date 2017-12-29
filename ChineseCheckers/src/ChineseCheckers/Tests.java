ce5551298d659d06954a8ae9ef9b498f01f4e4b4
grzesiek999999 <grzesiek999999@64f2ab8b-f227-451f-962c-9178d2ca13a8>
2017-12-29 14:24:58 +0000

    Added Junit tests

---
+++ ChineseCheckers/src/ChineseCheckers/Tests.java
@@ -0,0 +1,24 @@
+package ChineseCheckers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Tests {
+
+	@Test
+	public void test() {
+		
+	}
+
+
+
+	@Test
+	public void test2() {
+	
+	}
+
+}
+
+
+
