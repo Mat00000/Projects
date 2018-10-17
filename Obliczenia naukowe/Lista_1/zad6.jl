d6eef9c6543f7085af4f3d75d7aac8425521cfb9
Mateusz Laskowski <mati1003.ml@gmail.com>
2018-10-17 21:12:48 +0200

    komentarze

--- Obliczenia naukowe/Lista_1/zad6.jl
+++ Obliczenia naukowe/Lista_1/zad6.jl
@@ -3,11 +3,11 @@
 =#
 
 ################# OBLICZ W ARYTMETYCE FLOAT64 #################
-i = 1;
-f = Float64(0.0);
-g = Float64(0.0);
+i = 1;                                                                      # zmienna indeksowana
+f = Float64(0.0);                                                           # zmienna, której przypisze się wartości funckji f
+g = Float64(0.0);                                                           # zmienna, której przypisze się wartości funckji g
 while (i < 20)
-    x = Float64((1/8)^i);
+    x = Float64((1/8)^i);                                                   # zmienna 8^-i
     @printf("x = %e", x);
     println();
     f = Float64(sqrt(Float64(x^2) + Float64(1.0)) - Float64(1.0));
