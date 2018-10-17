43bef97c40b62709c9910990a85187c7bd9f819d
Mateusz Laskowski <mati1003.ml@gmail.com>
2018-10-17 19:45:32 +0200

    zadanie 2

---
+++ Obliczenia naukowe/Lista_1/zad2.jl
@@ -0,0 +1,27 @@
+#=
+#   @author Mateusz Laskowski
+=#
+
+# Float16
+println("Float16")
+epsilon = Float16(Float16(3*(Float16(4/3)-Float16(1)))-Float16(1));
+print("wynik: ")
+println(epsilon);
+print("eps: ");
+println(eps(Float16));
+
+# Float32
+println("Float32")
+epsilon = Float32(Float32(3*(Float32(4/3)-Float32(1)))-Float32(1));
+print("wynik: ")
+println(epsilon);
+print("eps: ");
+println(eps(Float32));
+
+# Float64
+println("Float64")
+epsilon = Float64(Float64(3*(Float64(4/3)-Float64(1)))-Float64(1));
+print("wynik: ")
+println(epsilon);
+print("eps: ");
+println(eps(Float64));
