fa454d4ff422a9e19893f3436bda3b176c59ee8c
Mateusz Laskowski <mati1003.ml@gmail.com>
2018-11-12 13:19:23 +0100

    zmiana i testy

--- Języki formalne/Lista nr 2/zadanie_3/input.c
+++ Języki formalne/Lista nr 2/zadanie_3/input.c
@@ -1,44 +1,57 @@
+%{
+#include <iostream>
+using namespace std;
+extern int yylex();
+bool flag = false;
+
+%}
+%option noyywrap
+%%
+"//".*							/** Ignoruj "one-line" comments */
+"//"(.*\\\n)*.*					/** Ignoruj "multi-line one-line comments" */
+
+"/**/"							/** Ignoruj "multi-line one-line comments" */
+"/**"[^*]+[.\n]*"*/"			{if(flag == true) ECHO;} 	/** Dokumentacja */
+"/*"[^*]+[.\n]*"*/"				{;}							/** Ignoruj "multi-line one-line comments" */
+
+%%
+
 int main(int argc, char* argv[]) {
-	const char option = 'd';
-	bool flag = false;
-	// Jeżeli argument wchodzący == 'd' to usuwamy
-	if(argc > 1) {
-		if(*(argv[1]) == option) {
+	char option; // zmienna
+	cout<<"T lub t pokaz dokumentacyjne.\n";
+	cin >> option;
+		if(option == 't' || option == 'T'){
 			flag = true;
 		}
-	}//
-	// 
-	/*
-	a normalnie 	
-	*/
-	
+		else {
+			flag = false;
+		}
+	// wpisuj się ///////////
+	////////////
+	cout<<"Flaga -> "<<flag<<"\n";
+
 	char name_file_input[] = "input.c";	
 	char name_file_output[] = "output.c";
-
+	/* ? tutaj wchodza /// */
+	/*
+	
+	*/
 	FILE *input_file = fopen(name_file_input, "r");
 	FILE *output_file = fopen(name_file_output, "wt");
 	
 	if(!input_file) {
 		printf("ERROR: Brak pliku inputowego\n");
 		return -1;
-	}// tutaj działa
-	/** Dokumentacyjny */
+	}
+	
 	yyin = input_file;
 	yyout = output_file;
 	yylex();
-	
+
 	fclose(input_file);
 	fclose(output_file);
-	/**
-		ciekawe
-	*/
+	// kczy /* */
 	printf("Ukoczono\n");
-	/* ta
-	*/
-	/*
-	cos jest napisane tak dla siebie ale jednak dokumentacyjnie
-	
-	*/
-	////////////////////
-	///
-	return 0;
+ 
+  return 0;
+}
--- Języki formalne/Lista nr 2/zadanie_3/output.c
+++ Języki formalne/Lista nr 2/zadanie_3/output.c
@@ -1,18 +1,39 @@
+%{
+#include <iostream>
+using namespace std;
+extern int yylex();
+bool flag = false;
+
+%}
+%option noyywrap
+%%
+"
+"
+
+""							
+"/**"[^*]+[.\n]*"*/"			{if(flag == true) ECHO;} 	
+"/*"[^*]+[.\n]*"*/"				{;}							
+
+%%
+
 int main(int argc, char* argv[]) {
-	const char option = 'd';
-	bool flag = false;
-	
-	if(argc > 1) {
-		if(*(argv[1]) == option) {
+	char option; 
+	cout<<"T lub t pokaz dokumentacyjne.\n";
+	cin >> option;
+		if(option == 't' || option == 'T'){
 			flag = true;
 		}
-	}
+		else {
+			flag = false;
+		}
 	
 	
-	
+	cout<<"Flaga -> "<<flag<<"\n";
+
 	char name_file_input[] = "input.c";	
 	char name_file_output[] = "output.c";
-
+	
+	
 	FILE *input_file = fopen(name_file_input, "r");
 	FILE *output_file = fopen(name_file_output, "wt");
 	
@@ -24,13 +45,11 @@ int main(int argc, char* argv[]) {
 	yyin = input_file;
 	yyout = output_file;
 	yylex();
-	
+
 	fclose(input_file);
 	fclose(output_file);
 	
 	printf("Ukoczono\n");
-	
-	
-	
-	
-	return 0;
+ 
+  return 0;
+}
