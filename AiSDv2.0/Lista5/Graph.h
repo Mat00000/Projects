a764e14fe5723efe51b893bc53a911ff17e10735
Mateusz Laskowski <mati1003.ml@gmail.com>
2019-06-02 18:10:35 +0200

    Wykres zad1, zad2 - algorytm

---
+++ AiSDv2.0/Lista5/Graph.h
@@ -0,0 +1,22 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+
+class Graph {
+	private:
+		int ** v1;
+		int ** v2;
+		int my_k;
+		int my_i;
+	public:
+		Graph(int k, int i);
+		~Graph();
+		void program();
+	protected:
+		int my_pow(int i);
+		int * my_rand(int max, int i);
+		void createV2(int k_2, int i);
+};
+
+
+#endif
